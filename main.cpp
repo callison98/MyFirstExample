59a9c27c85384e1047fc8715e3c3afbb1c94e0da
callison98 <callison4@mail.csuchico.edu>
2020-01-29 11:35:41 -0800

    input 2 numbers

--- main.cpp
+++ main.cpp
@@ -5,7 +5,11 @@ using std::cout;
 
 int main()
 {
+    int num1;
+    int num2;
     cout<<"Hi, please enter two numbers: ";
-
+    cin >> num1;
+    cin >> num2;
+    cout << "Here is number 1: " << num1 << " Here is number 2: " << num2;
     return 0;
 }
